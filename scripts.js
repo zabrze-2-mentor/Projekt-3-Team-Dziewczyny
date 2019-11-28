845a81e5e24190602ea9008e49751890cabb5867
EmiliaFlorek <emilaa000@gmail.com>
2019-11-28 22:21:23 +0100

    dodanie waluty do strony produktu

--- scripts.js
+++ scripts.js
@@ -1,54 +1,58 @@
-const endpoint = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'
+const endpoint =
+  "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
 //const endpoint = 'https://makeup-api.herokuapp.com/api/v1/products.json'
 
-const section = document.getElementById('produkt');
-let i = 0; 
+const section = document.getElementById("produkt");
+let i = 0;
 fetch(endpoint)
-    .then((data) => data.json())
-    .then(data => {
-        data.forEach(product => {
-			/*
+  .then(data => data.json())
+  .then(data => {
+    data.forEach(product => {
+      /*
 			Dodano:
 			 - Po 20 iteracjach wychodź z pętli 
 			 Zmieniono:
 			 - Link z mniejszą ilością produktów
 			Piotrek 
 			*/
-			i++; 
-			if(i>20){
-				return;
-			}
-			
-            const card = document.createElement("div")
-            
-            const name = document.createElement("h4");
-            name.textContent = product.name;
-            
-            const info = document.createElement("div");
+      i++;
+      if (i > 20) {
+        return;
+      }
 
-            const img = document.createElement("img");
-            img.setAttribute("src", product.image_link);
+      const card = document.createElement("div");
 
-            const category = document.createElement("h6");
-            category.textContent = product.product_type;
+      const name = document.createElement("h4");
+      name.textContent = product.name;
 
-            const price = document.createElement("h5");
-            price.textContent = product.price + product.price_sign;
+      const info = document.createElement("div");
 
-            const button = document.createElement("button");
-            button.setAttribute("class", "button");
-            button.textContent = "więcej";
-            button.addEventListener("click",(e) => {
-                window.location.href = "produkt.html?id=" + product.id
-            })
+      const img = document.createElement("img");
+      img.setAttribute("src", product.image_link);
 
-            section.appendChild(card)
-            card.appendChild(name);
-            card.appendChild(info);
-            info.appendChild(img);
-            info.appendChild(category);
-            info.appendChild(price);
-            info.appendChild(button);
-        });
-        
-    })
+      const category = document.createElement("h6");
+      category.textContent = product.product_type;
+
+      const price = document.createElement("h5");
+      if (product.price_sign == null) {
+        price.textContent = product.price + "USD";
+      } else {
+        price.textContent = product.price + product.price_sign;
+      }
+
+      const button = document.createElement("button");
+      button.setAttribute("class", "button");
+      button.textContent = "więcej";
+      button.addEventListener("click", e => {
+        window.location.href = "produkt.html?id=" + product.id;
+      });
+
+      section.appendChild(card);
+      card.appendChild(name);
+      card.appendChild(info);
+      info.appendChild(img);
+      info.appendChild(category);
+      info.appendChild(price);
+      info.appendChild(button);
+    });
+  });
